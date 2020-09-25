a8db00353e3af523fa5a98d944998ed985e7820e
Jeff Registre <jeff.registre@nytimes.com>
2020-09-25 08:32:15 -0400

    Alight top

--- src/ui/kanji_finder_ui.py
+++ src/ui/kanji_finder_ui.py
@@ -1,4 +1,5 @@
-from aqt.qt import QHBoxLayout, QGridLayout, Qt, QPushButton, QLineEdit
+from aqt.qt import QLayout, QHBoxLayout, QVBoxLayout, QGridLayout, Qt
+from aqt import qt
 
 
 class KanjiFinder(QHBoxLayout):
@@ -12,18 +13,23 @@ class KanjiFinder(QHBoxLayout):
 
 def kanji_scroller():
     grid_layout = QGridLayout()
-    search_field = QLineEdit("Hello")
+    search_field = qt.QLineEdit("Hello")
     grid_layout.addWidget(search_field, 0, 0, 1, 2)
 
     left_right_layout = QHBoxLayout()
     left_right_layout.setAlignment(Qt.AlignTop)
-    grid_layout.addLayout(left_right_layout, 1, 0, 1, 2)
+    grid_layout.addLayout(left_right_layout, 1, 0, 1, 2, Qt.AlignTop)
 
-    left_btn = QPushButton("<")
-    right_btn = QPushButton(">")
-    left_right_layout.addWidget(left_btn, 0)
-    left_right_layout.addWidget(right_btn, 0)
+    left_btn = qt.QPushButton("<")
+    right_btn = qt.QPushButton(">")
+    left_right_layout.addWidget(left_btn, 0, Qt.AlignTop)
+    left_right_layout.addWidget(right_btn, 0, Qt.AlignTop)
 
-    search_btn = QPushButton("Search")
-    grid_layout.addWidget(search_btn, 2, 0, 1, 2)
+    search_btn = qt.QPushButton("Search")
+    grid_layout.addWidget(search_btn, 2, 0, 1, 2, Qt.AlignTop)
     return grid_layout, search_field, left_btn, right_btn, search_btn
+
+
+def kanji_desc():
+    vbox = QVBoxLayout()
+    vbox.setAlignment(Qt.AlignTop)
