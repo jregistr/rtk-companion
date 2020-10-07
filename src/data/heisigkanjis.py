9e448b9ff874b938361cce7a662568f110fe908c
Jeff Registre <jeff.registre@nytimes.com>
2020-10-07 00:07:21 -0400

    messy experimenting

---
+++ src/data/heisigkanjis.py
@@ -0,0 +1,32 @@
+kanjis = [
+    {
+        "number": 212,
+        "kanji": "枠",
+        "keyword": "frame",
+        "stroke_order_fn": "paste-20dda0b5f63d565af1113dadb168c58336f1cfc0.jpg"
+    },
+    {
+        "number": 213,
+        "kanji": "梢",
+        "keyword": "treetops",
+        "stroke_order_fn": "paste-21de3cb1b4bcf42f503ba65b77ea1b219de9ef08.jpg"
+    },
+    {
+        "number": 214,
+        "kanji": "棚",
+        "keyword": "shelf",
+        "stroke_order_fn": "paste-25a37d1c0add01a52adf777de15756f9eb352b19.jpg"
+    },
+    {
+        "number": 215,
+        "kanji": "杏",
+        "keyword": "apricot",
+        "stroke_order_fn": "paste-31dffe009e71554dd18e6f824dd1aab5b195933f.jpg"
+    },
+    {
+        "number": 216,
+        "kanji": "桐",
+        "keyword": "paulownia",
+        "stroke_order_fn": "paste-44b50bfd3adbc3672bec0a5a469383e313bc8275.jpg"
+    }
+]
--- src/experiment.py
+++ src/experiment.py
@@ -1,27 +1,34 @@
-from aqt import gui_hooks
+from aqt import gui_hooks, mw, addcards
+from aqt.addcards import AddCards
 from aqt.editor import Editor
+from aqt.fields import FieldDialog
 from anki.hooks import wrap
+from anki.cards import Card
+from anki.notes import Note
 from typing import List, Any, Callable
 
 
+model_id = 1593059310637
+deck_id = 1591395939518
+collection = mw.col
+# note = Note(collection, {"id": model_id}, None)
+
+
 def html_buttons_call_back(info: List[Any]):
-    pass
+    with open("zooom-shaka.txt", "w", encoding="utf-8") as debug_fn:
+        debug_fn.write("WOOOOOWW")
+        debug_fn.flush()
 
 
 def add_button(editor: Editor, btn_text: str, btn_id: str, cmd: str, callback: Callable[[List[Any]], None]):
     html_text = """
     (() => {
-        let rendered = false;
-        if (!document.getElementById('%s')) {
-            document.getElementById('topbutsleft').innerHTML += `<button id='%s' onclick='pycmd("%s")'>%s</button> `; 
-        } else {
-            rendered = true;
-        }
-        return [rendered];
+        document.getElementById('topbutsleft').innerHTML += `<button id='%s' onclick='pycmd("%s")'>%s</button> `; 
+        return []
     })();
-    """ % (btn_id, btn_id, cmd, btn_text)
+    """ % (btn_id, cmd, btn_text)
 
-    editor.web.evalWithCallback(html_text, callback)
+    editor.web.evalWithCallback(html_text, html_buttons_call_back)
 
 
 def hook_for_editor_did_load_note(editor: Editor):
@@ -31,8 +38,44 @@ def hook_for_editor_did_load_note(editor: Editor):
     add_button(editor, "Koohie", "koohie-btn", "toggle_koohie", html_buttons_call_back)
 
 
-def command_from_html_btn_bridge(editor: Editor.onBridgeCmd, cmd: str):
-    pass
+def command_from_html_btn_bridge(editor: Editor, cmd: str):
+    if cmd == "run_fill_rtk":
+        maybe_add_cards: AddCards = editor.web.parent().parent()
+        assert (isinstance(maybe_add_cards, AddCards))
+
+        # with open("boom-shaka.txt", "a", encoding="utf-8") as debug_fn:
+        #     debug_fn.write(cmd)
+        #     debug_fn.write("yuuuups")
+        #     debug_fn.write("\n")
+        #     debug_fn.flush()
+        #     editor_note = editor.note
+        #     if editor.note:
+        #         debug_fn.write("Alright, note exists, \n")
+        #         editor_note.fields[0] = "foooBar"
+        #         editor_note.fields[1] = "zzDooo"
+        #         maybe_add_cards.form.fieldsArea.setStyleSheet("background-color:red;")
+        #         # from pprint import pprint
+        #         # varsz = dir(maybe_add_cards.form.fieldsArea)
+        #         # pprint(varsz, debug_fn)
+
+        def callback(info):
+            with open("boom-shaka.txt", "a", encoding="utf-8") as debug_fn:
+                debug_fn.write(str(info))
+                debug_fn.flush()
+
+        js = """
+                (() => {
+                var fields_inner = document.getElementById('fields').innerHTML; 
+                return fields_inner
+                })();
+                """
+        editor.web.evalWithCallback(js, callback)
+        # fields = editor_note.fields
+        # first_value = str(fields[0])
+        # debug_fn.write(first_value)
+
+    elif cmd == "toggle_koohie":
+        pass
 
 
 gui_hooks.editor_did_load_note.append(hook_for_editor_did_load_note)
