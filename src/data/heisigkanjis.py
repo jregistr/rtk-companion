7fa4dea93024182dcb70b7f4a9b7f01ac32cd8d2
Jeff Registre <jeff.registre@nytimes.com>
2020-10-10 20:29:59 -0400

    Stroke order files inside zipfile. Write media.

--- src/data/heisigkanjis.py
+++ src/data/heisigkanjis.py
@@ -1,14 +1,26 @@
-from json import load as load_json
 from typing import Dict, Optional
+from json import load as load_json
+from os import path
+from ..project import support_files_path
+from zipfile import ZipFile
+from aqt.utils import showWarning
+from anki.collection import Collection
 
 kanjis = None
 KANJI_DATA_FN = "kanji_data.json"
+KANJI_STRK_ZIP_FN = "stroke-orders.zip"
+
+KEY_NUMBER = "number"
+KEY_KANJI = "kanji"
+KEY_KEYWORD = "keyword"
+KEY_STROKE_ORDR = "stroke_order_fn"
 
 
 def load_kanjis():
     global kanjis
     if kanjis is None:
-        with open(KANJI_DATA_FN, "r") as kanji_db_json:
+        kanjis_fn_path = path.join(support_files_path, KANJI_DATA_FN)
+        with open(kanjis_fn_path, "r", encoding="utf-8") as kanji_db_json:
             kanjis = load_json(kanji_db_json)
 
 
@@ -18,5 +30,22 @@ def kanji_info_by_number(number: str) -> Optional[Dict[str, str]]:
 
     if kanjis is not None:
         return next((x for x in kanjis if x["number"] == number), None)
+    else:
+        return None
+
+
+def get_stroke_order_frame(kanji_info: Dict[str, str], col: Collection) -> Optional[str]:
+    strk_fn = kanji_info[KEY_STROKE_ORDR]
+    if strk_fn is not None:
+        zip_fn = path.join(support_files_path, KANJI_STRK_ZIP_FN)
+        try:
+            with ZipFile(zip_fn, "r") as strk_zip:
+                stroke_order_bytes = strk_zip.read(strk_fn)
+                media_frame = col.media.writeData(strk_fn, stroke_order_bytes)
+                return media_frame
+        except FileNotFoundError:
+            showWarning("Stroke order zip file not found at: %s" % zip_fn)
+        except KeyError:
+            showWarning("Looks like the stroke order image file %s is not in the archeive" % strk_fn)
     else:
         return None
--- src/data/rtkfill.py
+++ src/data/rtkfill.py
@@ -1,31 +1,41 @@
+from typing import Dict
 from aqt.editor import Editor
 from anki.notes import Note
-from aqt import mw, utils as anki_utils
-from .heisigkanjis import kanji_info_by_number
+from anki.collection import Collection
+from aqt import utils as anki_utils
+from . import heisigkanjis
+from .heisigkanjis import KEY_KEYWORD, KEY_NUMBER, KEY_KANJI
 
 
-MODEL_ID = 1593059310637
-
-
-def maybe_fill_editor_rtk_data(editor: Editor):
+def maybe_fill_editor_rtk_data(editor: Editor, col: Collection):
     editor_note = editor.note
     if editor_note is not None:
         number_f_value = editor_note["Number"]
         if number_f_value:
-            kanji_info = kanji_info_by_number(number_f_value)
-            new_editor_note = new_note()
-            new_editor_note["Number"] = number_f_value
-            new_editor_note["Character"] = kanji_info["kanji"]
-            new_editor_note["Keyword"] = kanji_info["keyword"]
-            st_img_fn = kanji_info["stroke_order_fn"]
-            new_editor_note["Stroke Order"] = '<img src="%s">' % st_img_fn
-            editor.setNote(new_editor_note)
+            kanji_info = heisigkanjis.kanji_info_by_number(number_f_value)
+            if kanji_info is not None:
+                new_editor_note = kanji_info_to_note(kanji_info, editor_note.mid, col)
+                editor.setNote(new_editor_note)
+            else:
+                anki_utils.showInfo("Could not find kanji info for heisig number: %s" % number_f_value)
         else:
             anki_utils.showInfo("No Number value found", type="warning")
     else:
         anki_utils.showInfo("Editor Note is None", type="critical")
 
 
-def new_note() -> Note:
-    col = mw.col
-    return Note(col, {"id": MODEL_ID}, None)
+def kanji_info_to_note(kanji_info: Dict, note_id: int, col: Collection) -> Note:
+    new_note = Note(col, {"id": note_id})
+    stroke_order = None
+    try:
+        stroke_order = heisigkanjis.get_stroke_order_frame(kanji_info, col)
+    except Exception:
+        anki_utils.showWarning("Unable to determine stroke order image")
+
+    new_note["Number"] = kanji_info[KEY_NUMBER]
+    new_note["Character"] = kanji_info[KEY_KANJI]
+    new_note["Keyword"] = kanji_info[KEY_KEYWORD]
+    if stroke_order is not None:
+        new_note["Stroke Order"] = '<img src="%s">' % stroke_order
+
+    return new_note
